2da5af83e672eb8693eb5f3d45d9fee2273ea6ec
Vizuara AI Lab <teamvizuara@gmail.com>
2025-09-29 14:41:41 +0530

    chore: add testing dependencies (Jest, playwright)
    Added the following dependencies for setting up the frontend testing framework:
    - @playwright/test
    - jest-environment-jsdom
    - ts-node
    
    ts-node resolved jest typescript error, jest-environment-jsdom was not included in jest by default.

---
+++ tests/setup/jest.setup.ts
@@ -0,0 +1,8 @@
+// Provides extended matchers like .toBeInTheDocument()
+import "@testing-library/jest-dom";
+
+// Optional: setup for MSW or Firebase mocks
+// import { server } from '../__mocks__/server'
+// beforeAll(() => server.listen())
+// afterEach(() => server.resetHandlers())
+// afterAll(() => server.close())
