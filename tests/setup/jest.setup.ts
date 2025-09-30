5801b7cdb57fb692577e631e56e7cb24048f3eaa
Vizuara AI Lab <teamvizuara@gmail.com>
2025-09-30 09:53:13 +0530

    chore: setup emulators and ts - changed firebase.json to handle emulators - add polyfill for fetch api - changed tsconfig.json for jest compatibility - deleted addDummyData.ts

--- tests/setup/jest.setup.ts
+++ tests/setup/jest.setup.ts
@@ -1,8 +1,2 @@
-// Provides extended matchers like .toBeInTheDocument()
 import "@testing-library/jest-dom";
-
-// Optional: setup for MSW or Firebase mocks
-// import { server } from '../__mocks__/server'
-// beforeAll(() => server.listen())
-// afterEach(() => server.resetHandlers())
-// afterAll(() => server.close())
+import 'cross-fetch/polyfill';
