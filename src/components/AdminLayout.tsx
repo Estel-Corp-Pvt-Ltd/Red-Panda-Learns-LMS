b1c8f2887a0594db608a4959395736239601b796
Gyanendra Baghel <gyanendrabaghel633@gmail.com>
2025-10-30 14:33:44 +0530

    add sidebar in submissions

--- src/components/AdminLayout.tsx
+++ src/components/AdminLayout.tsx
@@ -1,6 +1,6 @@
 import React, { ReactNode } from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import { LayoutDashboard, BookOpen, Users, Settings, Book, TicketPercent, Building2, ShoppingBag, UserPen, PictureInPicture } from 'lucide-react';
+import { LayoutDashboard, BookOpen, Users, Settings, Book, TicketPercent, Building2, ShoppingBag, UserPen, PictureInPicture, NotepadText } from 'lucide-react';
 import { Header } from './Header';
 import { cn } from '@/lib/utils';
 
@@ -32,6 +32,11 @@ const AdminLayout: React.FC<AdminLayoutProps> = ({ children }) => {
       name: 'Bundles',
       path: '/admin/bundles',
       icon: <Book className="h-5 w-5" />
+    },
+    {
+      name: 'Submissions',
+      path: '/admin/submissions',
+      icon: <NotepadText className="h-5 w-5" />
     },
     {
       name: 'Coupons',
