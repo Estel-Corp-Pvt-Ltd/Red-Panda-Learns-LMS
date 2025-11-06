66c6f8f070b98f1d5f802b21ad140c586f313f6d
Gyanendra Baghel <gyanendrabaghel633@gmail.com>
2025-11-06 14:56:09 +0530

    fix: enrolled courses

--- src/types/enrollment.ts
+++ src/types/enrollment.ts
@@ -1,42 +1,18 @@
 import { FieldValue, Timestamp } from "firebase/firestore";
 
-import {
-    EnrolledProgramType,
-    EnrollmentStatus,
-    PaymentStatus,
-    PricingModel,
-    UserRole
-} from "./general";
+import { EnrollmentStatus } from "./general";
 
 export interface Enrollment {
     id: string;
     userId: string;
-    targetId: string;                // courseId OR bundleId
-    targetType: EnrolledProgramType;
+    courseId: string;
+    courseName: string;
+    bundleId?: string;
     enrollmentDate: Timestamp | FieldValue;
     status: EnrollmentStatus;
-    role: UserRole;
-
-    // Always track main progress (for course = course’s progress, for bundle = overall aggregate)
-    progressId?: string;
-    progressSummary?: {
-        completedLessons?: number;
-        completedCourses?: number;
-        totalLessons?: number;
-        totalCourses?: number;
-        percent: number;
-    };
-    bundleProgress?: Array<{ courseId: string; progressId: string; }>;
-
-    pricingModel: PricingModel;
-    paymentSummary?: {
-        transactions?: string[]; // only include transaction IDs where status === SUCCESS
-        status: PaymentStatus;
-        totalAmount: number;
-        totalPaid: number;
-        balance: number;
-    };
+    orderId: string;
 
     createdAt: Timestamp | FieldValue;
     updatedAt: Timestamp | FieldValue;
 };
+
