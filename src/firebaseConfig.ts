1d69156fe8d6c24fc6122a83972b8da0698a7815
arbaaz-khan-tech <arbukhan1971@gmail.com>
2026-01-02 13:07:35 +0530

    fix(firebase-config): added messaging support

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -2,8 +2,9 @@ import { initializeApp } from "firebase/app";
 import { getFirestore,writeBatch } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
+import { getMessaging, isSupported } from "firebase/messaging";
 
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY_FIREBASE,
   authDomain: import.meta.env.VITE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_PROJECT_ID,
@@ -18,3 +19,15 @@ export const db = getFirestore(app);
 export const auth = getAuth(app);
 export const storage = getStorage(app);
 export const batch = writeBatch(db);
+
+/**
+ * Messaging must be guarded because:
+ * - SSR breaks it
+ * - Some browsers don’t support it
+ */
+export const messaging = await (async () => {
+  if (await isSupported()) {
+    return getMessaging(app);
+  }
+  return null;
+})();
